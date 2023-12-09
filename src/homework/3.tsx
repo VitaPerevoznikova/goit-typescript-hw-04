0f23665313e63f76243dd1477532aa6babbbd62d
VitaPerevoznikova <frayvita@gmail.com>
2023-12-09 19:47:40 +0200

    sum

--- src/homework/3.tsx
+++ src/homework/3.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 
+
 export function FormComponent() {
   const [value, setValue] = useState("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
@@ -11,3 +12,24 @@ export function FormComponent() {
     <input type="text" value={value} onChange={handleChange} />
   );
 }
+
+
+//import React, { useState, FormEvent, FC, ChangeEvent } from 'react';
+
+// interface ValueProps {
+//   value: (text: string) => void;
+// }
+
+// export const FormComponent: FC<ValueProps> = ({ value }) => {
+//   const [inputValue, setValue] = useState("");
+
+//   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+//     const newValue = event.target.value;
+//     setValue(newValue);
+//     value(newValue);
+//   };
+
+//   return (
+//     <input type="text" value={inputValue} onChange={handleChange} />
+//   );
+// };
